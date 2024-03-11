1a9418dee0e52a171f218aa9ebf8eafec54b9a33
GopikaVijayanS <153484878+gopikavijayans001@users.noreply.github.com>
2024-03-11 17:07:59 +0530

    update method implemented

--- src/main/java/com/ff/phonesimulatorapp/service/ContactService.java
+++ src/main/java/com/ff/phonesimulatorapp/service/ContactService.java
@@ -1,7 +1,6 @@
 package com.ff.phonesimulatorapp.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContextException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -9,6 +8,7 @@ import org.springframework.stereotype.Service;
 import com.ff.phonesimulatorapp.dao.ContactDao;
 import com.ff.phonesimulatorapp.dto.ResponseStructure;
 import com.ff.phonesimulatorapp.entity.Contact;
+import com.ff.phonesimulatorapp.exception.ContactNotFoundException;
 
 @Service
 public class ContactService {
@@ -24,9 +24,32 @@ public class ContactService {
 			response.setData(receivedContact);
 			response.setMessage("Contact Saved Suceesfully...!");
 			return new ResponseEntity<ResponseStructure<Contact>>(response, HttpStatus.CREATED);
-		} else
-			throw new ApplicationContextException("Contact not saved");
+		} else {
+			ResponseStructure<Contact> response = new ResponseStructure<Contact>();
+			response.setStatusCode(HttpStatus.BAD_REQUEST.value());
+			response.setMessage("Contact not saved...!");
+			return new ResponseEntity<ResponseStructure<Contact>>(response, HttpStatus.BAD_REQUEST);
+		}
 
+	}
+
+	public ResponseEntity<ResponseStructure<Contact>> editContact(int id, Contact contact) {
+		Contact recievedContact = contactDao.findContact(id);
+		if (recievedContact != null) {
+			Contact updatedContact = new Contact();
+			updatedContact.setContactName(contact.getContactName());
+			updatedContact.setContactnum(contact.getContactnum());
+			updatedContact.setContactGroup(contact.getContactGroup());
+			contactDao.saveContact(updatedContact);
+			ResponseStructure<Contact> response = new ResponseStructure<Contact>();
+			response.setStatusCode(HttpStatus.CREATED.value());
+			response.setData(updatedContact);
+			response.setMessage("Contact Saved Suceesfully...!");
+			return new ResponseEntity<ResponseStructure<Contact>>(response, HttpStatus.CREATED);
+
+		} else {
+			throw new ContactNotFoundException();
+		}
 	}
 
 }
