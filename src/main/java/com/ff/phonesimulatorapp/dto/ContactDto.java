9a3f716d4f5cc993c3bfde85330545ab6a76018e
Nakul240 <nakulnanjappa240@gmail.com>
2024-03-12 14:25:10 +0530

    call,endCall,message

--- src/main/java/com/ff/phonesimulatorapp/dto/ContactDto.java
+++ src/main/java/com/ff/phonesimulatorapp/dto/ContactDto.java
@@ -1,5 +1,13 @@
 package com.ff.phonesimulatorapp.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class ContactDto {
 
+	private String contactName;
+	private Long contactNumber;
+	private String message;
 }
