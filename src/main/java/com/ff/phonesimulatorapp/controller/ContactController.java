d8663912ef67f5877d550fd0656323aa0145c6c6
Nakul240 <153484923+Nakul240@users.noreply.github.com>
2024-03-15 15:36:00 +0530

    Update ContactController.java

--- src/main/java/com/ff/phonesimulatorapp/controller/ContactController.java
+++ src/main/java/com/ff/phonesimulatorapp/controller/ContactController.java
@@ -6,5 +6,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/contact")
 public class ContactController {
-
+  System.out.println("Hello World");
 }
